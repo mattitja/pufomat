08f22c293b784d9189cb5dfdf136c7db30de7ddc
Matze Matratze <9047940+mattitja@users.noreply.github.com>
2021-03-14 14:25:13 +0100

    cleaned up shit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var running = false;
 var floIsDran = true;
+var thinkText = "...";
+const interruptText = "(wurde abgewürgt)"
 
 function start() {
 	running = !running;
@@ -15,41 +17,12 @@ function start() {
 	} else {
 		const button = document.getElementById("button");
 		button.innerText = 'kann weiter gehn'
+		synth.cancel();
 	}
 	
 }
 
-function getFlorentin() {
-	return florentin[Math.floor(Math.random() * florentin.length)];
-}
-
-function getStefan() {
-	return stefan[Math.floor(Math.random() * stefan.length)];
-}
-
-function compare(promptsArray, repliesArray, string) {
-  let reply;
-  let replyFound = false;
-  for (let x = 0; x < promptsArray.length; x++) {
-    for (let y = 0; y < promptsArray[x].length; y++) {
-      if (promptsArray[x][y] === string) {
-        let replies = repliesArray[x];
-        reply = replies[Math.floor(Math.random() * replies.length)];
-        replyFound = true;
-        // Stop inner loop when input value matches prompts
-        break;
-      }
-    }
-    if (replyFound) {
-      // Stop outer loop when reply is found instead of interating through the entire array
-      break;
-    }
-  }
-  return reply;
-}
-
 function addFlorentin() {
-  
   floIsDran = false;
 	
   let product = getFlorentin();
@@ -62,23 +35,24 @@ function addFlorentin() {
   botImg.src = "flo_klein.png";
   botImg.className = "avatar";
   botDiv.className = "bot response";
-  botText.innerText = `...`;
+  botText.innerText = thinkText;
   botDiv.appendChild(botText);
   botDiv.appendChild(botImg);
   messagesContainer.appendChild(botDiv);
 
-  
-
-  // Fake delay to seem "real"
-  setTimeout(() => {
-	botText.innerText = `${product}`;
-	florentinVoice(product)
-	if (running) {
-		addStefan()
-	}
-  }, 3000
-  )
-  
+  if (running) {
+      setTimeout(() => {
+        if (running) {
+          botText.innerText = `${product}`;
+          florentinVoice(product)
+          addStefan()
+        } else {
+          botText.innerText = interruptText;
+        }
+      }, 3000)
+  } else {
+    botText.innerText = interruptText;
+  }
 
 }
 
@@ -93,20 +67,29 @@ function addStefan() {
   let userDiv = document.createElement("div");
   userDiv.id = "user";
   userDiv.className = "user response";
-  userDiv.innerHTML = `<img src="steff_klein.png" class="avatar"><span>...</span>`;
+  userDiv.innerHTML = `<img src="steff_klein.png" class="avatar"><span>${thinkText}</span>`;
   messagesContainer.appendChild(userDiv);
 
-
-  
-  // Fake delay to seem "real"
-  setTimeout(() => {
+  if (running) {
+    setTimeout(() => {
+      if (running) {
 	  userDiv.innerHTML = `<img src="steff_klein.png" class="avatar"><span>${product}</span>`;
-	stefanVoice(product)
-	if (running) {
-		addFlorentin()
-	}
-  }, 3000
-  )
-  
+	  stefanVoice(product)
+      addFlorentin()
+      } else {
+        userDiv.innerHTML = `<img src="steff_klein.png" class="avatar"><span>${interruptText}</span>`;
+      }
+    }, 3000)
+  } else {
+    userDiv.innerHTML = `<img src="steff_klein.png" class="avatar"><span>${interruptText}</span>`;
+  }
 
+}
+
+function getFlorentin() {
+	return florentin[Math.floor(Math.random() * florentin.length)];
+}
+
+function getStefan() {
+	return stefan[Math.floor(Math.random() * stefan.length)];
 }
